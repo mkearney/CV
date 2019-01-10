9fde9d522321b54b846dff5d1b916d3379fb068f
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-10 15:06:55 -0600

    add badge update to build

--- R/svg.R
+++ R/svg.R
@@ -2,13 +2,17 @@
 ## possible colors
 ##"brightgreen", "green", "yellowgreen", "yellow", "orange", "red", "lightgrey", "blue"
 
+download_file <- function(url, destfile) {
+  download.file(url, destfile, quiet = TRUE)
+}
+
 downloads_badge <- function(pkg, color, file = NULL) {
   url <- paste0(
     "https://cranlogs.r-pkg.org/badges/grand-total/",
     pkg, "?color=", color
   )
   tmp <- tempfile()
-  download.file(url, destfile = tmp)
+  download_file(url, destfile = tmp)
   x <- tfse::readlines(tmp)
   x <- sub('.*font-size="11"', 'font-size="16" font-family="\'PT Sans\'"', x)
   #x <- sub('font-family="DejaVu Sans,', 'font-family="Menlo"', x)
@@ -24,7 +28,7 @@ downloads_badge <- function(pkg, color, file = NULL) {
 zenodo_badge <- function(num, file = NULL) {
   url <- paste0("https://zenodo.org/badge/", num, ".svg")
   tmp <- tempfile()
-  download.file(url, destfile = tmp)
+  download_file(url, destfile = tmp)
   x <- tfse::readlines(tmp)
   x <- sub('.*font-size="11"', 'font-size="16"', x)
   x <- sub('font-family="DejaVu Sans,', 'font-family="\'PT Sans\'"', x)
@@ -37,30 +41,28 @@ zenodo_badge <- function(num, file = NULL) {
   invisible(file)
 }
 
-downloads_badge("rtweet", "yellowgreen", "img/rtweet-downloads.pdf")
-downloads_badge("textfeatures", "yellowgreen", "img/textfeatures-downloads.pdf")
-downloads_badge("tfse", "yellowgreen", "img/tfse-downloads.pdf")
-downloads_badge("tbltools", "yellowgreen", "img/tbltools-downloads.pdf")
-downloads_badge("funique", "yellowgreen", "img/funique-downloads.pdf")
-downloads_badge("pkgverse", "yellowgreen", "img/pkgverse-downloads.pdf")
+downloads_badge("rtweet", "yellowgreen", here::here("img", "rtweet-downloads.pdf"))
+downloads_badge("textfeatures", "yellowgreen", here::here("img", "textfeatures-downloads.pdf"))
+downloads_badge("tfse", "yellowgreen", here::here("img", "tfse-downloads.pdf"))
+downloads_badge("tbltools", "yellowgreen", here::here("img", "tbltools-downloads.pdf"))
+downloads_badge("funique", "yellowgreen", here::here("img", "funique-downloads.pdf"))
+downloads_badge("pkgverse", "yellowgreen", here::here("img", "pkgverse-downloads.pdf"))
 
+if (FALSE) {
+  dapr = "153846249"
+  pkgverse = "136514892"
+  tbltools  = "152122857"
+  tfse = "62493045"
+  funique = "133566034"
+  textfeatures = "123046986"
+  rtweet = "64161359"
 
-dapr = "153846249"
-pkgverse = "136514892"
-tbltools  = "152122857"
-tfse = "62493045"
-funique = "133566034"
-textfeatures = "123046986"
-rtweet = "64161359"
+  zenodo_badge(dapr, here::here("img", "dapr-doi.pdf"))
+  zenodo_badge(pkgverse, here::here("img", "pkgverse-doi.pdf"))
+  zenodo_badge(tbltools, here::here("img", "tbltools-doi.pdf"))
+  zenodo_badge(tfse, here::here("img", "tfse-doi.pdf"))
+  zenodo_badge(funique, here::here("img", "funique-doi.pdf"))
+  zenodo_badge(textfeatures, here::here("img", "textfeatures-doi.pdf"))
+  zenodo_badge(rtweet, here::here("img", "rtweet-doi.pdf"))
 
-zenodo_badge(dapr, "img/dapr-doi.pdf")
-zenodo_badge(pkgverse, "img/pkgverse-doi.pdf")
-zenodo_badge(tbltools, "img/tbltools-doi.pdf")
-zenodo_badge(tfse, "img/tfse-doi.pdf")
-zenodo_badge(funique, "img/funique-doi.pdf")
-zenodo_badge(textfeatures, "img/textfeatures-doi.pdf")
-zenodo_badge(rtweet, "img/rtweet-doi.pdf")
-
-
-
-
+}
