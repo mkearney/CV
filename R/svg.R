4bfe9e43601b09e79f44e3089c89f86710a8a18b
Michael W. Kearney <kearneymw@missouri.edu>
2019-02-14 14:41:52 -0600

    update

--- R/svg.R
+++ R/svg.R
@@ -54,46 +54,49 @@ zenodo_badge <- function(num, file = NULL) {
   invisible(file)
 }
 
-downloads_badge("dapr", "ff69b4",
-  here::here("img", "dapr-downloads.pdf"))
-downloads_badge("rtweet", "ff69b4",
-  here::here("img", "rtweet-downloads.pdf"))
-downloads_badge("textfeatures", "ff69b4",
-  here::here("img", "textfeatures-downloads.pdf"))
-downloads_badge("tfse", "ff69b4",
-  here::here("img", "tfse-downloads.pdf"))
-downloads_badge("tbltools", "ff69b4",
-  here::here("img", "tbltools-downloads.pdf"))
-downloads_badge("funique", "ff69b4",
-  here::here("img", "funique-downloads.pdf"))
-downloads_badge("pkgverse", "ff69b4",
-  here::here("img", "pkgverse-downloads.pdf"))
+if (grep("darwin", R.version$os)) {
 
-if (TRUE) {
-  dapr = "153846249"
-  pkgverse = "136514892"
-  tbltools  = "152122857"
-  tfse = "62493045"
-  funique = "133566034"
-  textfeatures = "123046986"
-  rtweet = "64161359"
+  downloads_badge("dapr", "ff69b4",
+    here::here("img", "dapr-downloads.pdf"))
+  downloads_badge("rtweet", "ff69b4",
+    here::here("img", "rtweet-downloads.pdf"))
+  downloads_badge("textfeatures", "ff69b4",
+    here::here("img", "textfeatures-downloads.pdf"))
+  downloads_badge("tfse", "ff69b4",
+    here::here("img", "tfse-downloads.pdf"))
+  downloads_badge("tbltools", "ff69b4",
+    here::here("img", "tbltools-downloads.pdf"))
+  downloads_badge("funique", "ff69b4",
+    here::here("img", "funique-downloads.pdf"))
+  downloads_badge("pkgverse", "ff69b4",
+    here::here("img", "pkgverse-downloads.pdf"))
 
-  zenodo_badge(dapr, here::here("img", "dapr-doi.pdf"))
-  zenodo_badge(pkgverse, here::here("img", "pkgverse-doi.pdf"))
-  zenodo_badge(tbltools, here::here("img", "tbltools-doi.pdf"))
-  zenodo_badge(tfse, here::here("img", "tfse-doi.pdf"))
-  zenodo_badge(funique, here::here("img", "funique-doi.pdf"))
-  zenodo_badge(textfeatures, here::here("img", "textfeatures-doi.pdf"))
-  zenodo_badge(rtweet, here::here("img", "rtweet-doi.pdf"))
-}
+  if (TRUE) {
+    dapr = "153846249"
+    pkgverse = "136514892"
+    tbltools  = "152122857"
+    tfse = "62493045"
+    funique = "133566034"
+    textfeatures = "123046986"
+    rtweet = "64161359"
+
+    zenodo_badge(dapr, here::here("img", "dapr-doi.pdf"))
+    zenodo_badge(pkgverse, here::here("img", "pkgverse-doi.pdf"))
+    zenodo_badge(tbltools, here::here("img", "tbltools-doi.pdf"))
+    zenodo_badge(tfse, here::here("img", "tfse-doi.pdf"))
+    zenodo_badge(funique, here::here("img", "funique-doi.pdf"))
+    zenodo_badge(textfeatures, here::here("img", "textfeatures-doi.pdf"))
+    zenodo_badge(rtweet, here::here("img", "rtweet-doi.pdf"))
+  }
 
 
-if (TRUE) {
-  cran_badge("dapr")
-  cran_badge("pkgverse")
-  cran_badge("tbltools")
-  cran_badge("tfse")
-  cran_badge("funique")
-  cran_badge("textfeatures")
-  cran_badge("rtweet")
+  if (TRUE) {
+    cran_badge("dapr")
+    cran_badge("pkgverse")
+    cran_badge("tbltools")
+    cran_badge("tfse")
+    cran_badge("funique")
+    cran_badge("textfeatures")
+    cran_badge("rtweet")
+  }
 }
